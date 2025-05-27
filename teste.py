ae41cbe4e7cae267e34ce0ff5dd5bb6715e204c0
Zezex18 <jmmcamaral@gmail.com>
2025-05-27 16:06:16 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import base64
 
 st.write('José Maurício')
 
@@ -10,5 +10,10 @@ st.write("""
 
 st.write("""Seja muito bem vindo, fique à vontade ;)""")
 
+file_ = open("astro-astrobot.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
 
-st.markdown("![Alt Text](astro-astrobot.gif)")
+st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,)
