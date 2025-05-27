34ce326e80401ff4ca36314d41b7fbf183bcb429
Zezex18 <jmmcamaral@gmail.com>
2025-05-27 15:21:47 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import streamlit as st
+import base64
+
 
 st.write('José Maurício')
 
@@ -8,3 +11,9 @@ st.write("""
 """)
 
 st.write("""Seja muito bem vindo, fique à vontade ;)""")
+
+"""### gif from local file"""
+file_ = open("file:///D:/user/C3008187/Downloads/astro-astrobot.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
