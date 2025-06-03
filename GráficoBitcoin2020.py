9b45658f33f28e8bb0ad78e2986c488772fb19fb
Zezex18 <jmmcamaral@gmail.com>
2025-06-03 18:56:25 -0300

    Update GráficoBitcoin2020.py

--- GráficoBitcoin2020.py
+++ GráficoBitcoin2020.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import streamlit as st
 
+try:
+  tot_registros = df.count()['total']
+except:
+  df = pd.read_csv('BTC-2020min.csv.zip')
+
+st.bar_chart(df, x='data', y='preço_fechamento')
